c7e8f88059f4facf74e9ba3e8d6eb9630319e3f4
Rice <rice@Rices-MacBook-Pro.local>
2020-07-31 18:15:40 +0700

    Choose option

--- Homework/src/homework/HomeWork.java
+++ Homework/src/homework/HomeWork.java
@@ -5,15 +5,47 @@ import java.util.Scanner;
 public class HomeWork {
 
 	public static void main(String[] args) {
-//		powerCalculate pc = new powerCalculate();
-//		System.out.print(pc.powerCalculate());
-//		divSeven dsf = new divSeven();
-//		dsf.divSevenNotFive();
-		checkPrime cp = new checkPrime();
-		cp.abc();
-		
+
+		int choose;
+		Scanner option = new Scanner(System.in);
+	
+		do {
+			showMenu();
+			System.out.println("********************");
+			System.out.println("Please choose option");
+			choose = Integer.parseInt(option.nextLine());
+			
+			switch (choose) {
+			case 1:
+
+				powerCalculate pc = new powerCalculate();
+				System.out.print(pc.powerCalculate());
+			break;
+			case 2:
+				divSeven dsf = new divSeven();
+				dsf.divSevenNotFive();
+			break;
+			case 3:
+				checkPrime cp = new checkPrime();
+				cp.abc();
+				break;
+				
+			case 4 :
+				System.exit(-1);
+				break;
+				default:
+					System.err.print("ERRROERRRR");
+			
+		}
+		}while(choose !=5);
+
 	}
 	
-	
-
+	static void showMenu() {
+		System.out.println("");
+		System.out.println("1.Tính giai thừa");
+		System.out.println("2. Chia hết cho 7 nhưng không chia hết cho 5 ");
+		System.out.println("3.Số nguyên tố ");
+		System.out.println("4. Exit");
+	}
 }
