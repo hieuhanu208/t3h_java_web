87827dde1cb718dfd08b58ca40cc4ab601543ea2
Rice <rice@Rices-MacBook-Pro.local>
2020-07-31 17:38:39 +0700

    Commit btvn 1

---
+++ Homework/src/homework/HomeWork.java
@@ -0,0 +1,19 @@
+package homework;
+
+import java.util.Scanner;
+
+public class HomeWork {
+
+	public static void main(String[] args) {
+//		powerCalculate pc = new powerCalculate();
+//		System.out.print(pc.powerCalculate());
+//		divSeven dsf = new divSeven();
+//		dsf.divSevenNotFive();
+		checkPrime cp = new checkPrime();
+		cp.abc();
+		
+	}
+	
+	
+
+}
